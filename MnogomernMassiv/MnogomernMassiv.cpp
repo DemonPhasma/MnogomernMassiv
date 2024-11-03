d03915c5a26744ed9891ba225e7471c07bf72c34
Бижова Андрей <andreybizhov@gmail.com>
2024-11-03 14:45:24 +0500

    Добавьте файлы проекта.

---
+++ MnogomernMassiv/MnogomernMassiv.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int rows, cols;
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> cols;
+
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; ++i) {
+        array[i] = new int[cols];
+    }
+
+    int number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (j == 0) {
+                array[i][j] = number;
+            }
+            else {
+                array[i][j] = array[i][j - 1] * 2; 
+            }
+        }
+    }
+
+    std::cout << "Созданный массив:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+
+    return 0;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int rows, cols;
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> cols;
+
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; ++i) {
+        array[i] = new int[cols];
+    }
+
+    int number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (j == 0) {
+                array[i][j] = number;
+            }
+            else {
+                array[i][j] = array[i][j - 1] + 1;
+            }
+        }
+    }
+
+    std::cout << "Созданный массив:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+
+    return 0;
+}
